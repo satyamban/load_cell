9ca1d00c0f96a87786473ef88afd8e4e98d4a2da
satyamban <37009554+satyamban@users.noreply.github.com>
2018-06-04 02:54:21 +0530

    Add files via upload

---
+++ load_basic_test0.ino
@@ -0,0 +1,19 @@
+
+#include "HX711.h"
+
+#define DOUT  3
+#define CLK  2
+ 
+HX711 scale(DOUT, CLK);
+
+void setup() 
+{
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  Serial.print(scale.get_units(), 3);
+  Serial.println();
+}
---
+++ load_cali_0.ino
@@ -0,0 +1,45 @@
+#include "HX711.h"
+
+#define DOUT  3
+#define CLK  2
+
+HX711 scale(DOUT, CLK);
+
+float cali=1.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  scale.set_scale();
+  scale.tare();
+  
+  
+}
+
+void loop() 
+{
+  Serial.println("Now place the known 10g weight and press C");
+  
+  while(!Serial.available())
+  {}
+  
+  char temp = Serial.read();
+  float wgt = 10.0;                        //NEEDS TO BE INITIALIZED 
+  float mes_wgt;
+  
+  if(temp == 'c' || temp == 'C')
+  {
+    
+    mes_wgt = scale.get_units(10);
+        
+    cali = cali*mes_wgt/wgt;
+    
+    scale.set_scale(cali);
+  }
+       
+   Serial.print("The value of cali is :");
+   Serial.println(cali);
+   Serial.print("The value of weight is :");
+   Serial.println(scale.get_units(), 3);
+   delay(2000);
+}
